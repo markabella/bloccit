5fa639709da012fae276e7d8ee0567e1776eb2e0
Mark Abella <mark.abella@gmail.com>
2017-07-01 13:39:27 +0000

    Checkpoint 28 Authorization complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,11 +48,19 @@
    )
  end
  
- # Test User
- user = User.first
- user.update_attributes!(
-   email: 'email@email.com',
-   password: 'password'
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
  )
  
  puts "Seed finished"
