68f431c567100f19e8413739f390f5813e76b012
Mark Abella <mark.abella@gmail.com>
2017-07-02 05:37:57 +0000

    Checkpoint 29 Comments complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701130839) do
+ActiveRecord::Schema.define(version: 20170702045522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 20170701130839) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -74,4 +76,5 @@ ActiveRecord::Schema.define(version: 20170701130839) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
