1aa68d8d753dc0a1cb4af8fc057cc858e5a25c98
Mark Abella <mark.abella@gmail.com>
2017-07-03 06:03:34 +0000

    Assignment 31 Favorites complete

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,27 @@
+class FavoriteMailer < ApplicationMailer
+   default from: "bloccit@email.com" #testing from email
+    
+   def new_comment(user, post, comment)
+ 
+     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+ 
+     @user = user
+     @post = post
+     @comment = comment
+ 
+     mail(to: user.email, subject: "New comment on #{post.title}")
+   end
+   
+   def new_post(post)
+     headers["Message-ID"] = "<post/#{post.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+ 
+     @post = post
+ 
+     mail(to: post.user.email, subject: "Following: #{post.title}")
+   
+   end
+end
